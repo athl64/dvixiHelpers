1e8312b11d65d9deb02ca1a5730d66462d0451bd
dvixi <athl64@bigmir.net>
2015-12-13 07:26:29 +0200

    fixing composer config

--- src/DvixiHelpers.php
+++ src/DvixiHelpers.php
@@ -8,8 +8,33 @@ namespace dvixi\src;
  * */
 class DvixiHelpers
 {
-    public static function echoTest()
+    /**
+     * Get value by it's key ( switch wrapper for array of cases )
+     *
+     * @param $key string
+     * @param $values array
+     * ['key1' => 'val1', 'key2' => 'val2']
+     * @param $defaultValue mixed
+     *
+     * @throws \Exception
+     *
+     * @return mixed
+     * */
+    public static function getValByKey( $key, $values = [], $defaultValue = null )
     {
-        echo 'Test complete.';
+        if( !is_array($values) ) {
+            throw new \Exception('Param is not array');
+        }
+
+        foreach( $values as $index => $value ) {
+            switch( $key ) {
+                case $index :
+                    return $value;
+                default :
+                    continue;
+            }
+        }
+
+        return $defaultValue;
     }
 }
